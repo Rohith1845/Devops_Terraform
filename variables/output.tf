e6922a4f72d562f6a4565fcf637a4fd804e2f8d8
rohith <krishnamsettyrohith@gmail.com>
2026-01-02 10:38:46 +0530

    success

--- variables/output.tf
+++
@@ -1,3 +0,0 @@
-# output "instance_output" {
-#     value = aws_instance.terraform
-# }
--- variables/s3bucket.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "new_bucket" {
-  bucket = "my_state_bucket"
-  tags = {
-    name = "my_new_bucket"
-    Environment = "state"
-  }
-
-}
